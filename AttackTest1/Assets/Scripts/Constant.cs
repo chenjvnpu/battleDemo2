53f1f7a93a642993c2c27ea7d643bead8386f73a
shichao.chen <shichao.chen@gaeamobile.com>
2017-07-21 11:13:16 +0800

    delete Test data

--- AttackTest1/Assets/Scripts/Constant.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Constant  {
-	public static float[] Formationx = new float[]{-1.5f,0,1.5f };
-	public static float[] Formationy = new float[]{0,1.5f,3 };
-}
